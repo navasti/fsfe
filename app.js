6ffc2542b81e0dbca563db16a1b99cf4e3a6bde9
Paweł Szostak <szostakpawel00@gmail.com>
2021-12-22 22:32:37 +0100

    testing statuses

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.get("/", (req, res) => {
   res.sendFile("views/test.html", { root: __dirname });
 });
 
+app.get("/teapot", (req, res) => {
+  res.status(418);
+  res.set("X-full-stack", "4life");
+  res.send("i preffer coffe");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
